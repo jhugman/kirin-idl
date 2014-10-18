42d98a03e21a48ba463d03b7af8eed6af7fbba62
James Hugman <james@hugman.tv>
2014-10-18 02:07:46 +0100

    Working with grunt

--- tasks/idl.js
+++ tasks/idl.js
@@ -1,33 +1,50 @@
 'use strict';
+var _ = require('lodash'),
+    fs = require('../lib/fs-helper'),
+    path = require('path');
 module.exports = function(grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
-  grunt.registerMultiTask('{%= short_name %}', '{%= description %}', function() {
-  // Merge task-specific and/or target-specific options with these defaults.
-  var options = this.options({
-    punctuation: '.',
-    separator: ', '
+  grunt.registerMultiTask('idl', '{%= description %}', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+
+      var dest = f.dest;
+      var lang = f.lang;
+
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      });
+
+      var translate = require('..');
+
+      var projMap = {};
+      projMap[lang] = dest;
+      var fileMap = translate(src, projMap);
+    
+      // Handle options.
+      // Write the destination file.
+      _.each(fileMap, function (i, filename) {
+        fs.writeFileSync(path.resolve(filename), grunt.util.normalizelf(fileMap[filename]));
+        grunt.log.writeln('File "' + filename + '" created.');
+      });
+      
+
+
+      // Print a success message.
+      
+    });
   });
-  // Iterate over all specified file groups.
-  this.files.forEach(function(f) {
-    // Concat specified files.
-    var src = f.src.filter(function(filepath) {
-    // Warn on and remove invalid source files (if nonull was set).
-    if (!grunt.file.exists(filepath)) {
-      grunt.log.warn('Source file "' + filepath + '" not found.');
-      return false;
-    } else {
-      return true;
-    }
-  }).map(function(filepath) {
-    // Read file source.
-    return grunt.file.read(filepath);
-  }).join(grunt.util.normalizelf(options.separator));
-  // Handle options.
-  src += options.punctuation;
-  // Write the destination file.
-  grunt.file.write(f.dest, src);
-  // Print a success message.
-  grunt.log.writeln('File "' + f.dest + '" created.');
-});
-});
+}
